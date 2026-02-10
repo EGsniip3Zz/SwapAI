0ade4dd26d5f9b4a82e13b7655042ee1e6708510
Ryan Reid <ryanreid2017@gmail.com>
2026-02-09 20:35:44 -0800

    Trigger redeploy

--- server/index.js
+++ server/index.js
@@ -56,3 +56,4 @@ app.post('/api/stripe/refresh-account-link', async (req, res) => {
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, '0.0.0.0', () => console.log('Server running on port ' + PORT))
+// force redeploy
